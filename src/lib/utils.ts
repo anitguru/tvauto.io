b2f868bcfd72f9d3deb917fceb1db5e0cafe1e87
Steve VanAllen <93944901+anitguru@users.noreply.github.com>
2025-01-12 15:39:37 -0500

    Delete src directory

--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-// src/lib/utils.ts
-
-/**
- * Convert an IPv4 string from dots to dashes.
- * e.g. "10.0.0.187" -> "10-0-0-187"
- */
-export function ipToDash(ip: string) {
-  return ip.replace(/\./g, "-");
-}
